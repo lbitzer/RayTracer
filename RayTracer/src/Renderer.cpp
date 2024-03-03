5c2a838f6f6115721f65f56c93fddc99ef295cb9
Lawrence Bitzer <lbitzer@umich.edu>
2024-03-02 19:11:36 -0500

    light refactoring (added renderer class)

---
+++ RayTracer/src/Renderer.cpp
@@ -0,0 +1,40 @@
+﻿#include "Renderer.h"
+#include "Walnut/Random.h"
+
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+
+    if (m_FinalImage)
+    {
+        if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+            return;
+
+        m_FinalImage->Resize(width, height);
+    }
+    else
+    {
+        m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+    }
+
+    delete[] m_ImageData;
+    m_ImageData = new uint32_t[width * height];
+    
+}
+
+void Renderer::Render()
+{
+    for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
+    {
+        m_ImageData[i] = Walnut::Random::UInt();
+        m_ImageData[i] |= 0xff000000;
+
+        // check if the point satisfys the equation with 1 or more intersections
+        // 1. Equation of the sphere, take the current position of the pixel, transform into (x,y) coordinate
+        // 2. Equation of the ray - Has to do with the camera? Every pixel can be used to create a ray
+        // 3. Discriminant to see if the ray intersects with the sphere or not
+    }
+
+    m_FinalImage->SetData(m_ImageData);
+
+}
--- RayTracer/src/WalnutApp.cpp
+++ RayTracer/src/WalnutApp.cpp
@@ -1,11 +1,11 @@
-#include <glm/fwd.hpp>
 
+#include "Renderer.h"
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+
 
 using namespace Walnut;
 
@@ -28,8 +28,9 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), {static_cast<float>(m_Image->GetWidth()), static_cast<float>(m_Image->GetHeight())});
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), {static_cast<float>(image->GetWidth()), static_cast<float>(image->GetHeight())});
 		
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -40,27 +41,14 @@ public:
 	void Render()
 	{
 		Timer timer;
+
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 		
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-	
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-		{
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-
-		m_Image->SetData(m_ImageData);
-
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0.0f;
