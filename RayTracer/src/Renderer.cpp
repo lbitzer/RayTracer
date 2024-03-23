e35bbbae84be274c8134e351ba3f0f31e14e55ef
Lawrence Bitzer <lbitzer@umich.edu>
2024-03-23 14:51:11 -0400

    Visualized hit points of our sphere using color coding and normalization

--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -1,6 +1,19 @@
 ﻿#include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace Utils
+{
+    static uint32_t ConvertToRGBA(const glm::vec4& color)
+    {
+        uint8_t r = (uint8_t)(color.r * 255.0f);
+        uint8_t g = (uint8_t)(color.g * 255.0f);
+        uint8_t b = (uint8_t)(color.b * 255.0f);
+        uint8_t a = (uint8_t)(color.a * 255.0f);
+
+        uint32_t result = (a << 24) | (b <<16) | (g << 8) | r;
+        return result;
+    }
+}
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
@@ -30,14 +43,16 @@ void Renderer::Render()
         {
             glm::vec2 coord = {x / static_cast<float>(m_FinalImage->GetWidth()), y / static_cast<float>(m_FinalImage->GetHeight())};
             coord = coord * 2.0f - 1.0f; // remap from 0->1 to -1->1
-            m_ImageData[y*m_FinalImage->GetWidth() + x] =  PerPixel(coord);
+            glm::vec4 color = PerPixel(coord);
+            color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f)); // manual clamping
+            m_ImageData[y*m_FinalImage->GetWidth() + x] = Utils::ConvertToRGBA(color);
         }
     }
     m_FinalImage->SetData(m_ImageData);
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
     // check if the point satisfys the equation with 1 or more intersections
     // 1. Equation of the sphere, take the current position of the pixel, transform into (x,y) coordinate
@@ -45,7 +60,7 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
     // 3. Discriminant to see if the ray intersects with the sphere or not
     
     glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
-    glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+    glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
     float radius = 0.5f;
 
     // Equation for sphere: (bx^2 + by^2)t^2 + 2(axbx + ayby)t + (ax^2 + ay^2 - r^2) = 0
@@ -62,8 +77,24 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
     // discriminant: b^2 - 4ac
     float discriminant = b * b - 4.0f * a * c;
 
-    if (discriminant >= 0)
-        return 0xffff00ff;
+    if (discriminant < 0.0f)
+    {
+        return glm::vec4(0, 0, 0, 1);
+    }
 
-    return 0xff000000;
+    glm::vec3 sphereColor(1, 0, 1);
+    
+    // Rest of Quadratic Formula
+
+    float t0 = (-b + sqrt(discriminant)) / (2.0f * a);
+    float t1 = (-b - sqrt(discriminant)) / (2.0f * a); // Since a is calculated by taking the dot product with itself, it will always be position, therefore this t will always be the smallest and therefore closest one.
+
+    // Calculating Hit Positions (a + bt)
+    //glm::vec3 h0 = rayOrigin + rayDirection * t0;
+    glm::vec3 hitPoint = rayOrigin + rayDirection * t1;
+    glm::vec3 normal = glm::normalize(hitPoint);
+
+    sphereColor = normal * 0.5f + 0.5f; // Converts range from -1 to 1 into 0 to 1
+        
+    return glm::vec4(sphereColor, 1.0f);
 }
